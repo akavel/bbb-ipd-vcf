27e0f0c047587965f47885e3db405e42e8b1ab79
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2013-08-19 23:35:32 +0200

    bbb-dat.lua: parse records and fields

--- bbb-dat-lua/bbb-dat.lua
+++ bbb-dat-lua/bbb-dat.lua
@@ -3,6 +3,9 @@
 -- explorations with a hex editor
 
 function bigendian(s)
+	if s==nil then
+		return nil
+	end
 	local t = {s:byte(1, #s)}
 	local n = 0
 	for i = 1, #t do
@@ -11,6 +14,9 @@ function bigendian(s)
 	return n
 end
 function littleendian(s)
+	if s==nil then
+		return nil
+	end
 	local t = {s:byte(1, #s)}
 	local n = 0
 	for i = 1, #t do
@@ -22,47 +28,79 @@ function sprintf(fmt, ...)
 	return fmt:format(...)
 end
 
-function parse(f)
-	local readn = function(n) return f:read(n) end
-	local read8 = function() return f:read(1):byte() end
-	local read16be = function() return bigendian(f:read(2)) end
-	local read16le = function() return littleendian(f:read(2)) end
-	local read32le = function() return littleendian(f:read(4)) end
-	local readname = function() return readn(read16le()) end
-	local clearname = function(name, namesep)
-		assert(#name>0)
-		assert(name:sub(-1):byte() == namesep)
-		return name:sub(1, #name-1)
-	end
+local readn = function(f, n) return f:read(n) end
+local read8 = function(f) return bigendian(f:read(1)) end
+local read16be = function(f) return bigendian(f:read(2)) end
+local read16le = function(f) return littleendian(f:read(2)) end
+local read32le = function(f) return littleendian(f:read(4)) end
+local readname = function(f) return readn(f, read16le(f)) end
+local clearname = function(name, namesep)
+	assert(#name>0)
+	assert(name:sub(-1):byte() == namesep)
+	return name:sub(1, #name-1)
+end
 
+function stringreader(s)
+	return setmetatable({}, {__index = {
+		read = function(self, n)
+			if #s==0 then
+				return nil
+			end
+			local head = s:sub(1, n)
+			s = s:sub(n+1)
+			return head
+		end
+	}})
+end
+
+function parse(f)
 	-- file header
 	local magic = "Inter@ctive Pager Backup/Restore File\n"
-	assert(readn(#magic) == magic, "incorrect magic ID")
-	assert(read8() == 2, "incorrect version")
-	local numdb = read16be()
+	assert(readn(f, #magic) == magic, "incorrect magic ID")
+	assert(read8(f) == 2, "incorrect version")
+	local numdb = read16be(f)
 	assert(numdb == 1, sprintf("incorrect numdb, expected 1, got %d; FIXME", numdb))
-	local namesep = read8()
+	local namesep = read8(f)
 	assert(namesep == 0, "incorrect namesep")
 	
-	local readclearname = function() return clearname(readname(), namesep) end
+	local readclearname = function(f) return clearname(readname(f), namesep) end
 	
 	-- database names
 	for i = 1, numdb do
-		print(sprintf('DB %d "%s"', i-1, readclearname()))
+		print(sprintf('DB %d "%s"', i-1, readclearname(f)))
 	end
-	--[[
+	
 	-- database records
+	local recs = {}
 	while true do
-		local dbId = read16le()
-		local recLen = read32le()
+		local dbId = read16le(f)
+		local recLen = read32le(f)
 		
-		_ = readn(7) -- drop some data which seems not useful to us {Ver uint8; Rhandle uint16; Ruid uint32}
+		_ = readn(f, 7) -- drop some data which seems not useful to us {Ver uint8; Rhandle uint16; Ruid uint32}
 		recLen = recLen - 7
-		while recLen > 0 do
+		rec = parseRec(readn(f, recLen))
+		for _, v in ipairs(rec) do
 			
+			--print(sprintf("0x%02x %q", v.kind, v.value))
 		end
+		recs[#recs+1] = rec
 	end
-	]]
+end
+
+function parseRec(buf)
+	local f = stringreader(buf)
+	local t = {}
+	while true do
+		local fieldlen = read16le(f)
+		local kind = read8(f)
+		local value = readn(f, fieldlen)
+		
+		if fieldlen == nil then
+			return t
+		end
+
+		t[#t+1] = {kind=kind, value=value}
+	end
 end
 
 function main()
